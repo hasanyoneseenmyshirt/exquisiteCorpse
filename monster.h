e0f1153a7ae2b59d0a90ca325ad7d609fd360d85
hasanyoneseenmyshirt <45132601+hasanyoneseenmyshirt@users.noreply.github.com>
2020-09-06 08:26:37 -0700

    Create monster.h

---
+++ monster.h
@@ -0,0 +1,16 @@
+#include<string>
+
+void MonsterHead();
+void MonsterBody();
+void MonsterFeet();
+
+void GhostHead();
+void GhostBody();
+void GhostFeet();
+
+
+void BugHead();
+void BugBody();
+void BugFeet();
+
+void CreateACreature();
